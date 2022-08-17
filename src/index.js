d2633224fa73a149ca7e4be288b0ebea60fd2c8f
Mohit <mohkum9045@gmail.com>
2022-08-17 15:49:10 +0530

    Adding base code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let date = new Date(2020, 8, 8, 18    );
+date = date.getHours();
+var greeting = "";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+if (date >= 1 && date <= 12) {
+  greeting = "Good Morning !"
+}
+else if (date > 12 && date < 19) {
+  greeting = "Good Evening !"
+}
+else {
+  greeting = "Good Night !"
+}
+
+ReactDOM.render(
+<>
+  <div style={{ backgroundColor: "#808000", textAlign: "center", width:"100%", height:"100%", justifyContent:"center",alignItems:"center" }}>
+    <h2 style={{ color: "#FFA500" }}>Hii Mohit, {greeting}</h2>
+    </div>
+</>
+    ,document.getElementById("root")
+)
+
+
